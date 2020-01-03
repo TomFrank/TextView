c19215d06764b7e773021cda74967cc6ca47b8b5
Tom Frank <zzj97114@gmail.com>
2020-01-03 14:35:07 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 
 let package = Package(
 	name: "TextView",
-	products: [
+    platforms: [
+       .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
 		.library(
 			name: "TextView",
 			targets: ["TextView"]
